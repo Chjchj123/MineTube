54355fb4b59cd1b57a5b447fcb3863dd5e0e5efe
Chjchj123 <quanganh2004dasuo@gmail.com>
2025-02-27 23:32:19 +0700

    'configindexdb'

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 async function connect(){
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/dqa_nodejs_dev1');
+
+        mongoose.connect('mongodb://127.0.0.1:27017/dqa_nodejs_dev1', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 150000 // Tăng timeout lên 15 giây
+          })
+          .then(() => console.log("MongoDB connected"))
+          .catch(err => console.error("MongoDB connection error:", err));
         console.log('Connection Successfully!');
     } catch (error) {
         console.log('Connection Failed!');
