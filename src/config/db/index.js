4d55b33ec4a0bc8764f397ef3bc764eabdfc086d
Chjchj123 <quanganh2004dasuo@gmail.com>
2025-02-28 08:37:51 +0700

    'finish-1.0'

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect(){
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/dqa_nodejs_dev1');
+        await mongoose.connect('mongodb+srv://dqa0604:dqa0604@cluster.jtbsa.mongodb.net/dqa_nodejs_dev1?retryWrites=true&w=majority&appName=Cluster');
         console.log('Connection Successfully!');
     } catch (error) {
         console.log('Connection Failed!');
