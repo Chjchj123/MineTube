303c5bb5bf4ab9526012f71d18c00508a6858ebd
Chjchj123 <quanganh2004dasuo@gmail.com>
2025-03-03 14:23:12 +0700

    'test-6'

--- src/app/controllers/MiddlewareController.js
+++ src/app/controllers/MiddlewareController.js
@@ -5,16 +5,15 @@ class middlewareController {
     verifyToken(req, res, next){
         const token = req.cookies.refreshToken;
         if(token){
-            const accessToken = token.split(" ")[1];
             jwt.verify(token, "dqa20062004", (err, user)=>{
                 if(err){
-                    res.redirect('auth/login');
+                    console.error("JWT Verification Error:", err.message);
+                    return res.redirect('auth/login');
                 }
                 req.username = user;
                 next();
             });
         }else{
-            res.json('Lỗi tại đây');
             res.redirect('auth/login');
         }
     }
